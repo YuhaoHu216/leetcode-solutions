60d818e3f251f3e2389c19c7e00abc904415e6f5
HYH <huyuhao612@outlook.com>
2025-07-31 16:03:31 +0800

    第13题:最大子数组和

---
+++ normal/quesiton10/question13/Solution.java
@@ -0,0 +1,26 @@
+package quesiton10.question13;
+
+/**
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 子数组是数组中的一个连续部分。
+ * 示例 1：
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ */
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int max = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            currentSum = Math.max(nums[i],nums[i] + currentSum);
+            max = Math.max(currentSum,max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(new Solution().maxSubArray(nums));
+    }
+}
