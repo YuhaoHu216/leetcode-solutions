f238750ec7b5a4802f06184c5fcc85b2eceea02b
HYH <huyuhao612@outlook.com>
2025-08-02 14:18:12 +0800

    第20题:旋转图像

---
+++ normal/question20/Solution.java
@@ -0,0 +1,34 @@
+package question20;
+
+/**
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ * 示例 1：
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[7,4,1],[8,5,2],[9,6,3]]
+ */
+public class Solution {
+    public void rotate(int[][] matrix) {
+        // 先对称变换,每行再反转
+        int n = matrix.length;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        int left = 0, right = n - 1;
+        while(left < right){
+            for(int i = 0; i < n; i++){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+            }
+            left++;
+            right--;
+        }
+    }
+}
