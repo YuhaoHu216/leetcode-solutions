d9a428b126886ef4090905578301c3d4e77a3210
HYH <huyuhao612@outlook.com>
2025-08-02 17:22:55 +0800

    第22题:相交链表

---
+++ easy/question22/Solution.java
@@ -0,0 +1,52 @@
+package question22;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode temp = headA;
+        while(temp != null){
+            set.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while(temp != null){
+            if(set.contains(temp)){
+                return temp;
+            }
+            temp = temp.next;
+        }
+
+        return null;
+    }
+}
+
+// 用双指针来实现
+class Solution2 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode pA = headA, pB = headB;
+
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+
+        return pA;
+    }
+}
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+          val = x;
+          next = null;
+     }
+}
