0989ede2f308c06df56ba7727e96517e2692bfe8
HYH <huyuhao612@outlook.com>
2025-08-03 17:05:54 +0800

    第27题:合并有序链表

---
+++ easy/question27/Solution.java
@@ -0,0 +1,50 @@
+package question27;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+// 迭代做法
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode first = new ListNode(-1);
+        ListNode pre = first;
+        while(list1 != null &&  list2 != null){
+            if(list1.val <= list2.val){
+                pre.next = list1;
+                list1 = list1.next;
+            }else{
+                pre.next = list2;
+                list2 = list2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = list1 == null ? list2 : list1;
+        return first.next;
+
+    }
+}
+
+// 递归做法
+class Solution2 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+
+        }
+    }
+}
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
