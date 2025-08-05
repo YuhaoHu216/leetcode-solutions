a2e7f416d99b1d59e9e562ae096ed3414a3d7057
HYH <huyuhao612@outlook.com>
2025-08-05 15:35:30 +0800

    第33题:排序链表(数据量大用归并排序)

---
+++ normal/question33/Solution.java
@@ -0,0 +1,54 @@
+package question33;
+
+/**
+ * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ * 示例 1：
+ * 输入：head = [4,2,1,3]
+ * 输出：[1,2,3,4]
+ * 数据量很大，插入排序会超时，用归并排序
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        // 1. 快慢指针找中点
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;         // 记录 slow 的上一个节点
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;  // 切断链表为两个部分
+
+        // 2. 递归排序左右部分
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        // 3. 合并两个有序链表
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), p = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        p.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
