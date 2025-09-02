ae7af83385342e0ff747a476cbc140aa8c081a72
HYH <huyuhao612@outlook.com>
2025-09-02 22:41:16 +0800

    第45题:二叉树的右视图

---
+++ medium/question45/Solution.java
@@ -0,0 +1,51 @@
+package question45;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的右视图
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        if(root != null){
+            queue.offer(root);
+        }
+        while(!queue.isEmpty()){
+            LinkedList<Integer> level = new LinkedList<>();
+            int n = queue.size();
+            for(int i = 0; i < n; i++){
+                TreeNode poll = queue.poll();
+                level.push(poll.val);
+                if(poll.left != null){
+                    queue.offer(poll.left);
+                }
+                if(poll.right != null){
+                    queue.offer(poll.right);
+                }
+            }
+            result.add(level.pop());
+
+        }
+        return result;
+
+    }
+}
