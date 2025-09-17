ce66917690d9bb6544535d74b9039d9462810d77
HYH <huyuhao612@outlook.com>
2025-09-17 20:53:55 +0800

    第70题:最小栈

---
+++ medium/question70/MinStack.java
@@ -0,0 +1,50 @@
+package question70;
+
+import java.util.LinkedList;
+
+/**
+ * 最小栈
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * 实现 MinStack 类:
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ */
+class MinStack {
+
+    // 所谓最小栈就是每个元素存储其值的时候也要存当前的最小值
+
+    private LinkedList<int[]> stack = new LinkedList<int[]>();
+
+    public MinStack() {
+
+        stack.push(new int[]{0,Integer.MAX_VALUE});
+    }
+
+    public void push(int val) {
+        stack.push(new int[]{val,Math.min(getMin(),val)});
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
