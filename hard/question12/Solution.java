9ed61af7018bcc569d0b844045bdb0985691bf43
HYH <huyuhao612@outlook.com>
2025-10-04 11:23:24 +0800

    第12题:最小覆盖字串

--- hard/question12/Solution.java
+++ hard/question12/Solution.java
@@ -1,4 +1,74 @@
 package question12;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 最小覆盖字串
+ * 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+ * 注意：
+ * 对于 t 中重复字符，我们寻找的子字符串中该字符数量必须不少于 t 中该字符数量。
+ * 如果 s 中存在这样的子串，我们保证它是唯一的答案。
+ * 示例 1：
+ * 输入：s = "ADOBECODEBANC", t = "ABC"
+ * 输出："BANC"
+ * 解释：最小覆盖子串 "BANC" 包含来自字符串 t 的 'A'、'B' 和 'C'。
+ * 示例 2：
+ * 输入：s = "a", t = "a"
+ * 输出："a"
+ * 解释：整个字符串 s 是最小覆盖子串。
+ * 示例 3:
+ * 输入: s = "a", t = "aa"
+ * 输出: ""
+ * 解释: t 中两个字符 'a' 均应包含在 s 的子串中，
+ * 因此没有符合条件的子字符串，返回空字符串。
+ */
+class Solution {
+    public String minWindow(String s, String t) {
+        // 记录需要的字符
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int valid = 0;
+        // 记录最小覆盖子串的起始索引和长度
+        int start = 0, len = Integer.MAX_VALUE;
+
+        while (right < s.length()) {
+            // 扩大窗口
+            char c = s.charAt(right);
+            right++;
+            // 更新 window 中的统计数据
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            // 当满足要求时，尝试收缩窗口
+            while (valid == need.size()) {
+                // 更新最小窗口
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                // 移出 left 指针的字符
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
 }
+
