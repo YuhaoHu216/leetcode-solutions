3593f246b74f182d494a862bb9a8c4ef748300d5
HYH <huyuhao612@outlook.com>
2025-10-06 11:52:43 +0800

    第47题:从前序和中序遍历序列构造二叉树

--- medium/question47/Solution.java
+++ medium/question47/Solution.java
@@ -1,8 +1,66 @@
 package question47;
 
-/**
- * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
- */
-public class Solution {
+import java.util.HashMap;
 
+/**
+ * 47.从前序和中序遍历序列构造二叉树
+ * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历，
+ * inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ */
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        // 将中序遍历的结果放入哈希表中方便获取根节点
+        HashMap<Integer,Integer> inMap = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++){
+            inMap.put(inorder[i],i);
+        }
+
+        return build(preorder,0,preorder.length-1,inorder,0,inorder.length-1,inMap);
+    }
+
+    private TreeNode build(int[] preorder,int preStart,int preEnd,
+                           int[] inorder,int inStart,int inEnd,HashMap<Integer,Integer> inMap){
+
+        // 终止条件
+        if(preStart > preEnd || inStart > inEnd) return null;
+
+        // 获取根节点及其中序遍历索引
+        int rootValue = preorder[preStart];
+        int rootIndex = inMap.get(rootValue);
+        TreeNode root = new TreeNode(rootValue);
+
+        // 根据索引求出左子树节点数
+        int leftSize = rootIndex - inStart;
+
+
+        // 中序遍历特点:根节点左边为左子树节点,右边为右子树节点
+        // 前序遍历特点:根节点后面紧跟左子树节点,然后是右子树节点
+
+        // 构造左子树,主要参数有左子树节点在前,中序遍历的索引范围
+        root.left = build(preorder,preStart+1,preStart+leftSize,
+                inorder,inStart,rootIndex-1,inMap);
+
+        // 构造右子树,主要参数有右子树节点在前,中序遍历的索引范围
+        root.right = build(preorder,preStart+leftSize+1,preEnd,
+                inorder,rootIndex+1,inEnd,inMap);
+
+        return root;
+
+    }
 }
