686f38f2ff48d5c8cede73fd0c0ea84c8c0c0be3
HYH <huyuhao612@outlook.com>
2025-10-10 20:01:19 +0800

    第90题:最长有效括号

---
+++ hard/question90/Solution.java
@@ -0,0 +1,42 @@
+package question90;
+
+import java.util.LinkedList;
+
+/**
+ * 32. 最长有效括号
+ * 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号 子串 的长度。
+ * 左右括号匹配，即每个左括号都有对应的右括号将其闭合的字符串是格式正确的，比如 "(()())"。
+ * 示例 1：
+ * 输入：s = "(()"
+ * 输出：2
+ * 解释：最长有效括号子串是 "()"
+ * 示例 2：
+ * 输入：s = ")()())"
+ * 输出：4
+ * 解释：最长有效括号子串是 "()()"
+ * 示例 3：
+ * 输入：s = ""
+ * 输出：0
+ */
+class Solution {
+    public int longestValidParentheses(String s) {
+        int result = 0;
+        // 用来存储字符下标
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.push(-1);   // 注意初始化
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();     // 注意数据弹出
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    result = Math.max(result, i - stack.peek());
+                }
+            }
+        }
+
+        return result;
+    }
+}
