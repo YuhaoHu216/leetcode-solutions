4a4a1ff6615c47c8fddbdb07df1944372a4fe308
HYH <huyuhao612@outlook.com>
2025-10-13 14:24:37 +0800

    第9题:找到字符串中所有字母异位词(二刷)

--- medium/question09/Solution.java
+++ medium/question09/Solution.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * 438.找到字符串中所有字母异位词
  * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
  * 示例 1:
  * 输入: s = "cbaebabacd", p = "abc"
