7dc566ea1bc794f6d70db12d9062f5b2b4c50819
HYH <huyuhao612@outlook.com>
2025-10-25 14:02:58 +0800

    第24题:回文链表(二刷)

--- easy/question24/Solution.java
+++ easy/question24/Solution.java
@@ -38,3 +38,49 @@ class ListNode {
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
+
+class Solution2 {
+    public boolean isPalindrome(ListNode head) {
+        // 最优解思路,快慢指针找终点,将右半链表翻转,再依次比较
+        // 不最优解可以将值存入动态数组然后左右双指针比较
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        // fast为null就说明有偶数个节点,否则为奇数个节点
+        // 当为奇数个节点时,slow指针要多跳一个(不要中心节点)
+        if(fast != null){
+            slow = slow.next;
+        }
+
+        ListNode p1 = head;
+        ListNode p2 = reverse(slow);
+
+        while(p2 != null){
+            if(p1.val != p2.val){
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return true;
+    }
+
+    // 反转链表
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
