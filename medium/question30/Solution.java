e4b72bfe790400a39432941c94b3fbf928438d77
HYH <huyuhao612@outlook.com>
2025-10-31 16:41:34 +0800

    第30题:两两交换链表中的节点(二刷)

--- medium/question30/Solution.java
+++ medium/question30/Solution.java
@@ -5,13 +5,15 @@ package question30;
  */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
+        // 迭代法,维护每一个段的前一个节点
         ListNode first = new ListNode(-1,head);
         ListNode pre = first;
 
         while(pre.next != null && pre.next.next != null ){
+            // 获得要交换的两个结点
             ListNode node1 = pre.next;
             ListNode node2 = pre.next.next;
-
+            // 执行交换的逻辑
             pre.next = node2;
             node1.next = node2.next;
             node2.next = node1;
