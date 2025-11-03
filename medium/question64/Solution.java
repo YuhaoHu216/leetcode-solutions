1df85c8e18488a73ab76980a2b934788c5b86e11
HYH <huyuhao612@outlook.com>
2025-11-03 15:48:27 +0800

    第64题:搜索二维矩阵(二刷)

--- medium/question64/Solution.java
+++ medium/question64/Solution.java
@@ -6,6 +6,7 @@ package question64;
  * 每行中的整数从左到右按非严格递增顺序排列。
  * 每行的第一个整数大于前一行的最后一个整数。
  * 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
+ *
  */
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
