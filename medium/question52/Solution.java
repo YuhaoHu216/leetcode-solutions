6ac9dd09d36f0b734e76f4bccfed9dbfa06bb4f1
HYH <huyuhao612@outlook.com>
2025-11-07 18:25:58 +0800

    第52题:腐烂的橘子(二刷)

--- medium/question52/Solution.java
+++ medium/question52/Solution.java
@@ -3,7 +3,7 @@ package question52;
 import java.util.LinkedList;
 
 /**
- * 腐烂的橘子
+ * 994.腐烂的橘子
  * 在给定的 m x n 网格 grid 中，每个单元格可以有以下三个值之一：
  * 值 0 代表空单元格；
  * 值 1 代表新鲜橘子；
@@ -11,9 +11,7 @@ import java.util.LinkedList;
  * 每分钟，腐烂的橘子 周围 4 个方向上相邻 的新鲜橘子都会腐烂。
  * 返回 直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1 。
  */
-/**
- * 994. 腐烂的橘子
- */
+
 class Solution {
     public int orangesRotting(int[][] grid) {
 
