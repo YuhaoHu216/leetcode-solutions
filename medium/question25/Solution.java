20a915afa36bc0a0df1430bda19ade02ed1143a4
HYH <huyuhao612@outlook.com>
2025-11-11 23:44:52 +0800

    LeetCode:25.三数之和

--- medium/question25/Solution.java
+++ medium/question25/Solution.java
@@ -1,4 +1,4 @@
-package question06;
+package question25;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,32 +20,45 @@ import java.util.List;
  *
  * tag:双指针
  */
-public class Solution {
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
-//        int left,right = nums.length - 1;
-        for(int i = 0;i < nums.length - 2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int right = nums.length - 1;
+
+        // 因为三元组不能重复,所以要避免之前的数参与后面的组合,这里的length-2是一个考量
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 去除重复的数(重复数只判定第一个)
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            // left = i + 1也是一个去重的考量
             int left = i + 1;
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0){
-                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[right], nums[left], nums[i]));
+                    // 更新指针的值,并且去重
                     left++;
                     right--;
-                    while(left < right && nums[left] == nums[left -1]) left++;
-                    while(left < right && nums[right] == nums[right + 1]) right--;
-                }else if(sum < 0){
-                    left++;
-                }else{
+                    // 去重应该是移动指针,并且是while,而非continue
+                    while (nums[left] == nums[left - 1] && left < right)
+                        left++;
+                    while (nums[right] == nums[right + 1] && left < right)
+                        right--;
+                } else if (sum > 0) {
                     right--;
+                } else {
+                    left++;
                 }
             }
         }
         return result;
+
     }
+
 
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
