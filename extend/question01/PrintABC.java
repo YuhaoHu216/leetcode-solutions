c0ae5562945c8dfccd3e37240e54db4410bf96c4
HYH <huyuhao612@outlook.com>
2026-02-04 23:49:26 +0800

    番外:1.按顺序打印ABC

---
+++ extend/question01/PrintABC.java
@@ -0,0 +1,41 @@
+package question01;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintABC {
+
+    private static int state = 0;
+    private static Lock lock = new ReentrantLock();
+
+    private static Condition c1 = lock.newCondition();
+    private static Condition c2 = lock.newCondition();
+    private static Condition c3 = lock.newCondition();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> print("A", 0, c1, c2)).start();
+        new Thread(() -> print("B", 1, c2, c3)).start();
+        new Thread(() -> print("C", 2, c3, c1)).start();
+    }
+
+    private static void print(String str, int target,
+                              Condition current, Condition next) {
+        for (int i = 0; i < 10; i++) {
+            lock.lock();
+            try {
+                while (state % 3 != target) {
+                    current.await();
+                }
+                System.out.print(str);
+                state++;
+                next.signal();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
