5a0ebb77a84d24d26e28faa64d2b0e6bad67ac14
HYH <huyuhao612@outlook.com>
2026-03-04 13:56:09 +0800

    LeetCode: 1143.最长公共子序列 (I)

--- medium/question1143/Solution.java
+++ medium/question1143/Solution.java
@@ -18,6 +18,7 @@ package question1143;
  * 输入：text1 = "abc", text2 = "def"
  * 输出：0
  * 解释：两个字符串没有公共子序列，返回 0 。
+ *
  */
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
