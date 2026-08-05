5bb6bb649d4bf2eb8baf0c9e017f87695093206f
HYH <huyuhao612@outlook.com>
2026-08-05 22:34:12 +0800

    LeetCode: 240.搜索二维矩阵II (备战秋招)

--- medium/question240/Solution.java
+++ medium/question240/Solution.java
@@ -2,10 +2,13 @@ package question240;
 
 /**
  * 240.搜索二维矩阵II
- *
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
  */
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        // 从左下角或者右上角开始搜索 注意 matrix[i][j] 的起始i j值决定是左下还是右上
         int m = matrix.length; // 行数
         int n = matrix[0].length; // 列数
         int row = 0;  // 行索引
