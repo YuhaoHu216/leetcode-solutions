ca70e4f7bde92652512ec9af1c4703b7f4acad13
HYH <huyuhao612@outlook.com>
2026-08-07 22:21:10 +0800

    LeetCode: 24.两两交换链表中的节点 (备战秋招)

--- medium/question30/Solution.java
+++ medium/question30/Solution.java
@@ -2,6 +2,8 @@ package question30;
 
 /**
  * 24.两两交换链表中的节点
+ * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+ * 你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
  */
 public class Solution {
     public ListNode swapPairs(ListNode head) {
@@ -14,7 +16,7 @@ public class Solution {
             ListNode node1 = pre.next;
             ListNode node2 = pre.next.next;
             // 执行交换的逻辑
-            pre.next = node2;
+            pre.next = node2;   // 链表要接上
             node1.next = node2.next;
             node2.next = node1;
             pre = node1;
