1bcea5b63377ed865a50c977c917096c8cd9c7b7
HYH <huyuhao612@outlook.com>
2026-08-07 22:03:19 +0800

    LeetCode: 19.删除链表的倒数第N个结点 (备战秋招)

--- medium/question19/Solution.java
+++ medium/question19/Solution.java
@@ -1,13 +1,44 @@
 package question19;
 
-
-
 /**
  * 19.删除链表的倒数第N个结点
  * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
- *
  */
-public class Solution {
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        // 注意这里不要new 新节点，否则不是一条链表
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        // 两次循环可以用一次代替
+        while(fast != null){
+            fast = fast.next;
+            if(n < 0){
+                slow = slow.next;
+            }
+            n--;
+        }
+
+//        // fast 先走 n 步
+//        for (int i = 0; i < n; i++) {
+//            fast = fast.next;
+//        }
+//
+//        // slow 和 fast 一起走
+//        while (fast.next != null) {
+//            slow = slow.next;
+//            fast = fast.next;
+//        }
+
+        // 删除 slow 后面的节点
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
+
+class Solution2 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
             // 注意从前置节点开始遍历 防止只有一个节点的情况
