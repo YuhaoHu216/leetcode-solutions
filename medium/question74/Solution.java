513d82ed635e01ef8c16a17ac0aa5d8fbd182ab9
HYH <huyuhao612@outlook.com>
2026-08-29 17:30:02 +0800

    LeetCode: 74.搜索二维矩阵 (备战秋招)

--- medium/question74/Solution.java
+++ medium/question74/Solution.java
@@ -8,6 +8,7 @@ package question74;
  * 给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
  *
  */
+// 该解法时间复杂度为 O(m+n)
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
@@ -28,3 +29,65 @@ class Solution {
         return false;
     }
 }
+
+// 该解法时间复杂度为O(log(m * n))
+// 两次二分（先第一列二分找到最后一个不大于目标值的元素，然后在该元素所在行中二分查找目标值是否存在）
+class Solution2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rowIndex = binarySearchFirstColumn(matrix, target);
+        if (rowIndex < 0) {
+            return false;
+        }
+        return binarySearchRow(matrix[rowIndex], target);
+    }
+
+    public int binarySearchFirstColumn(int[][] matrix, int target) {
+        int low = -1, high = matrix.length - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (matrix[mid][0] <= target) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean binarySearchRow(int[] row, int target) {
+        int low = 0, high = row.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
+
+// 把每一行拼接成一行就可以用一次二分得到答案
+class Solution3 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            // 得到原矩阵中的值
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
