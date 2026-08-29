97525ddbec3508540511629988c2c7fbe74f0d74
HYH <huyuhao612@outlook.com>
2026-08-29 17:16:25 +0800

    LeetCode: 35.搜索插入位置 (备战秋招)

--- easy/question35/Solution.java
+++ easy/question35/Solution.java
@@ -19,7 +19,7 @@ public class Solution {
     public int searchInsert(int[] nums, int target) {
 
         int left = 0;
-        int right = nums.length;
+        int right = nums.length - 1;
 
         while(left <= right){
             int mid = left + (right - left) / 2;
--- medium/question131/Solution.java
+++ medium/question131/Solution.java
@@ -1,4 +1,4 @@
-package question61;
+package question131;
 
 import java.util.ArrayList;
 import java.util.List;
