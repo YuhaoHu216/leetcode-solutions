c72581199189a033f52d6dda514042d9de18ce8d
HYH <huyuhao612@outlook.com>
2026-09-07 15:39:49 +0800

    LeetCode: 45.跳跃游戏II 03min23s (备战秋招)

--- medium/question45/Solution.java
+++ medium/question45/Solution.java
@@ -8,27 +8,8 @@ package question45;
  * i + j < n
  * 返回到达 n - 1 的最小跳跃次数。测试用例保证可以到达 n - 1。
  */
+
 class Solution {
-    public int jump(int[] nums) {
-
-        int start = 0;
-        int rightmost = 1;
-        int result = 0;
-
-        while(rightmost < nums.length){
-            int nextmost = 0;
-            for(int i = start; i < rightmost; i++){
-                nextmost = Math.max(nextmost,nums[i] + i);
-            }
-            start = rightmost;
-            rightmost = nextmost+1;
-            result++;
-        }
-
-        return result;
-    }
-}
-class Solution2 {
     public int jump(int[] nums) {
         // 给每一个段设置一个终点,维护到右边的最大值
         int end = 0;
@@ -46,4 +27,25 @@ class Solution2 {
         return result;
     }
 }
+
+class Solution2 {
+    public int jump(int[] nums) {
+        int start = 0;
+        int rightmost = 1;
+        int result = 0;
+
+        while(rightmost < nums.length){
+            int nextmost = 0;
+            for(int i = start; i < rightmost; i++){
+                nextmost = Math.max(nextmost,nums[i] + i);
+            }
+            start = rightmost;
+            rightmost = nextmost+1;
+            result++;
+        }
+
+        return result;
+    }
+}
+
 
